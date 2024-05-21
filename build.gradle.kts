78c7baa4adcf78ef0a5685bf957933781997521f
phunghoanglong <119026015+phunghoanglong@users.noreply.github.com>
2024-05-21 22:23:22 +0700

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.gms.google-services") version "4.4.1" apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.google.gms:google-services:4.4.1")
+        // Add the Firebase Crashlytics Gradle plugin
+        classpath("com.google.firebase:firebase-crashlytics-gradle:2.8.1")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
